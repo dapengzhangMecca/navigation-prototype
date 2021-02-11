b5ea96baa145f067cde8c04d5514a6607386c013
Dapeng Zhang <dapeng.zhang@mecca.com.au>
2021-02-11 17:44:16 +1100

    add mocked data

---
+++ src/data/mockedCategories.js
@@ -0,0 +1,341 @@
+export const mockedCategories = [
+  {
+    name: 'New',
+    subCategories: [
+      { name: 'New Skincare', subCategories: [] },
+      { name: 'New Makeup', subCategories: [] },
+      { name: 'New Fragrance', subCategories: [] },
+      { name: 'New Hair Care', subCategories: [] },
+      { name: 'New Body & Personal Care', subCategories: [] },
+      { name: "New Gifts, Sets & Mini's", subCategories: [] },
+    ],
+  },
+  {
+    name: 'Brands',
+    subCategories: [
+      { name: 'Online Exclusives', subCategories: [] },
+      { name: 'Cruelty Free Brands', subCategories: [] },
+      { name: 'A-Beauty Brands', subCategories: [] },
+      { name: 'Skincare Brands', subCategories: [] },
+      { name: 'Makeup Brands', subCategories: [] },
+      { name: 'Fragrance Brands', subCategories: [] },
+      { name: 'Hair Care Brands', subCategories: [] },
+      { name: 'Body & Personal Care Brands', subCategories: [] },
+    ],
+  },
+  {
+    name: 'Skincare',
+    subCategories: [
+      { name: 'Discover Skincare Brands', subCategories: [] },
+      { name: 'Vegan Skincare', subCategories: [] },
+      { name: 'Skincare Sets', subCategories: [] },
+      {
+        name: 'Cleansers',
+        subCategories: [
+          { name: 'Cream & Milk Cleansers' },
+          { name: 'Gel & Foaming Cleansers' },
+          { name: 'Oil & Balm Cleansers' },
+          { name: 'Makeup Remover' },
+        ],
+      },
+      {
+        name: 'Exfoliators & Peels',
+        subCategories: [{ name: 'Facial Scrub' }, { name: 'Facial Peels' }],
+      },
+      { name: 'Facial Toners', subCategories: [] },
+      {
+        name: 'Serums & Essences',
+        subCategories: [{ name: 'Serums' }, { name: 'Essences' }],
+      },
+      {
+        name: 'Treatments',
+        subCategories: [
+          { name: 'Acne & Blemish Solutions' },
+          { name: 'Aging' },
+          { name: 'Eczema' },
+          { name: 'Pigmentation' },
+          { name: 'Pregnancy' },
+          { name: 'Redness' },
+          { name: 'Rosacea' },
+          { name: 'Scarring' },
+        ],
+      },
+      {
+        name: 'Moisturisers & Oils',
+        subCategories: [
+          { name: 'Moisturiser' },
+          { name: 'Facial Oils' },
+          { name: 'Night Cream' },
+          { name: 'Tinted Moisturiser & BB Creams' },
+        ],
+      },
+      { name: 'Facial Sunscreen', subCategories: [] },
+      { name: 'Face Mists', subCategories: [] },
+      {
+        name: 'Masks',
+        subCategories: [
+          { name: 'Sheet Mask' },
+          { name: 'Clay & Mud Masks' },
+          { name: 'Overnight Masks' },
+        ],
+      },
+      {
+        name: 'Eye Care',
+        subCategories: [
+          { name: 'Eye Creams' },
+          { name: 'Eye Serums' },
+          { name: 'Undereye Masks' },
+        ],
+      },
+      { name: 'Lip Care', subCategories: [] },
+      { name: 'Hand Cream', subCategories: [] },
+      { name: 'Tanning', subCategories: [] },
+      {
+        name: 'Devices',
+        subCategories: [{ name: 'Electronic Devices' }, { name: 'Tools' }],
+      },
+    ],
+  },
+  {
+    name: 'Makeup',
+    subCategories: [
+      { name: 'Discover Makeup Brands', subCategories: [] },
+      {
+        name: 'Vegan Makeup',
+        subCategories: [
+          { name: 'Vegan Makeup' },
+          { name: 'Vegan Makeup Brushes' },
+        ],
+      },
+      {
+        name: 'Face & Complexion',
+        subCategories: [
+          { name: 'Primer' },
+          { name: 'Tinted Moisturiser & BB Creams' },
+          { name: 'Foundation' },
+          { name: 'Concealer' },
+          { name: 'Setting Powders' },
+          { name: 'Setting Sprays' },
+          { name: 'Complexion Brushes & Sponges' },
+        ],
+      },
+      {
+        name: 'Cheeks & Contour',
+        subCategories: [
+          { name: 'Blush' },
+          { name: 'Bronzer & Contour' },
+          { name: 'Highlighter & Illuminator' },
+          { name: 'Complexion Palettes & Sets' },
+        ],
+      },
+      {
+        name: 'Eye Makeup',
+        subCategories: [
+          { name: 'Eye Primer' },
+          { name: 'Eyeshadow Palettes & Singles' },
+          { name: 'Eyeliner' },
+          { name: 'Mascara' },
+          { name: 'False Eyelashes' },
+          { name: 'Eyelash Treatments' },
+        ],
+      },
+      {
+        name: 'Eyebrow Makeup',
+        subCategories: [
+          { name: 'Brow Pencil' },
+          { name: 'Brow Pomades & Shadows' },
+          { name: 'Brow Gel' },
+        ],
+      },
+      {
+        name: 'Lips',
+        subCategories: [
+          { name: 'Lip Liner' },
+          { name: 'Lipstick' },
+          { name: 'Lip Balms' },
+          { name: 'Lip Gloss & Oils' },
+          { name: 'Lip Sets' },
+        ],
+      },
+      {
+        name: 'Makeup Brushes & Tools',
+        subCategories: [
+          { name: 'Complexion Brushes & Sponges' },
+          { name: 'Eyeshadow Brushes' },
+          { name: 'Brow & Lip Brushes' },
+          { name: 'Brush Sets' },
+          { name: 'Makeup Tools' },
+        ],
+      },
+      { name: 'Makeup Remover', subCategories: [] },
+      { name: 'Nails', subCategories: [] },
+      { name: 'Foundation Finder', subCategories: [] },
+    ],
+  },
+  {
+    name: 'Fragrance',
+    subCategories: [
+      { name: 'Discover Fragrance Brands', subCategories: [] },
+      {
+        name: 'Fragrance & Perfumes',
+        subCategories: [
+          { name: 'Mini Fragrance & Roll Ons' },
+          { name: 'Fragrance Sets' },
+          { name: 'Hair & Body Fragrance Mists' },
+        ],
+      },
+      { name: "Men's Fragrance & Cologne", subCategories: [] },
+      {
+        name: 'Body Fragrance',
+        subCategories: [
+          { name: 'Hand Cream' },
+          { name: 'Hand & Body Wash' },
+          { name: 'Hand & Body Lotion' },
+        ],
+      },
+      {
+        name: 'Candles & Home Scents',
+        subCategories: [
+          { name: 'Candles' },
+          { name: 'Room Sprays & Diffusers' },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Hair Care',
+    subCategories: [
+      { name: 'Discover Hair Care Brands', subCategories: [] },
+      {
+        name: 'Shampoo & Conditioner',
+        subCategories: [
+          { name: 'Shampoo' },
+          { name: 'Conditioner' },
+          { name: 'Dry Shampoo' },
+        ],
+      },
+      {
+        name: 'Hair Treatments',
+        subCategories: [
+          { name: 'Hair Masks' },
+          { name: 'Leave-in Treatments' },
+          { name: 'Scalp Treatments' },
+          { name: 'Hair Colour Maintenance' },
+          { name: 'Hair Oils & Serums' },
+        ],
+      },
+      {
+        name: 'Hair Styling & Care',
+        subCategories: [
+          { name: 'Heat Protectant' },
+          { name: 'Hair Sprays' },
+          { name: 'Texturizing & Volumizing' },
+          { name: 'Hair Gels, Mousses & Waxes' },
+          { name: 'Curl Defining' },
+        ],
+      },
+      {
+        name: 'Shop by Hair Concern',
+        subCategories: [
+          { name: 'Frizz Prone Hair' },
+          { name: 'Oily Hair' },
+          { name: 'Dry & Sensitive Hair' },
+          { name: 'Fine Hair' },
+          { name: 'Dull Hair' },
+        ],
+      },
+      { name: 'Hair Styling Tools', subCategories: [] },
+      { name: 'Hair Brushes & Combs', subCategories: [] },
+      { name: 'Hair Kits & Sets', subCategories: [] },
+      { name: 'Hair Accessories', subCategories: [] },
+      { name: 'Book a Hair Consult', subCategories: [] },
+    ],
+  },
+  {
+    name: 'Body & Personal Care',
+    subCategories: [
+      { name: 'Discover Body & Personal Care Brands', subCategories: [] },
+      {
+        name: 'Hand & Body Wash',
+        subCategories: [
+          { name: 'Hand & Body Wash' },
+          { name: 'Hand Soap & Sanitizer' },
+        ],
+      },
+      {
+        name: 'Body Treatments',
+        subCategories: [
+          { name: 'Cellulite' },
+          { name: 'Dry Skin' },
+          { name: 'Scarring' },
+          { name: 'Stretch Marks' },
+        ],
+      },
+      { name: 'Body Scrubs & Exfoliators', subCategories: [] },
+      {
+        name: 'Body Lotions & Oils',
+        subCategories: [
+          { name: 'Body Lotions & Moisturizer' },
+          { name: 'Body Oils' },
+        ],
+      },
+      {
+        name: 'Hand & Nail Care',
+        subCategories: [{ name: 'Hand Creams' }, { name: 'Nails' }],
+      },
+      { name: 'Sunscreen', subCategories: [] },
+      { name: 'Tanning', subCategories: [] },
+      {
+        name: 'Personal Care',
+        subCategories: [
+          { name: 'Oral Care' },
+          { name: 'Shaving' },
+          { name: 'Deodorant' },
+          { name: 'Supplements' },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Gifts, Sets & Mini's",
+    subCategories: [
+      { name: 'Gift Cards & E-Gift Cards', subCategories: [] },
+      {
+        name: 'Gifts By Category',
+        subCategories: [
+          { name: 'Skincare Gifts' },
+          { name: 'Makeup Gifts' },
+          { name: 'Fragrance Gifts' },
+          { name: 'Hair Gifts' },
+          { name: 'Body & Personal Care Gifts' },
+          { name: 'Vegan Gifts' },
+        ],
+      },
+      {
+        name: 'Gifts By Price',
+        subCategories: [
+          { name: 'Gifts Under $25' },
+          { name: 'Gifts $25 - $50' },
+          { name: 'Gifts $50 - $75' },
+          { name: 'Gifts $75 - $100' },
+          { name: 'Gifts $100 - $150' },
+        ],
+      },
+      {
+        name: "Mini's & Travel Size",
+        subCategories: [
+          { name: "Skincare Mini's" },
+          { name: "Makeup Mini's" },
+          { name: "Fragrance Mini's" },
+          { name: "Hair Mini's" },
+          { name: "Body & Personal Care Mini's" },
+          { name: "Vegan Mini's" },
+        ],
+      },
+      { name: 'Corporate Gifts', subCategories: [] },
+    ],
+  },
+  { name: 'Services & Events', subCategories: [] },
+  { name: 'MECCA Live', subCategories: [] },
+  { name: 'The MEMO', subCategories: [] },
+]
